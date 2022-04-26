34b3d5eab6ae76dc814d0a87327cbed6cf1df703
Razztha <razztha111@gmail.com>
2022-04-26 17:40:42 +0530

    fourth

--- app/src/composables/useWorkspace.js
+++ app/src/composables/useWorkspace.js
@@ -2,7 +2,8 @@ import { computed } from 'vue'
 import { useAnchorWallet } from 'solana-wallets-vue'
 import { Connection, PublicKey } from '@solana/web3.js'
 import { AnchorProvider, Program } from '@project-serum/anchor'
-import idl from '../../../target/idl/solana_extractor.json'
+// import idl from '../../../target/idl/solana_extractor.json'
+import idl from '@/idl/solana_twitter.json'
 
 const programID = new PublicKey(idl.metadata.address)
 let workspace = null
