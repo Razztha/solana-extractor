63e8590babb9983700acbb0cfbd6ef390abc0278
Razztha <razztha111@gmail.com>
2022-05-13 14:36:41 +0530

    Test fullsreen 5

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { useRoute } from 'vue-router'
-import TheSidebar from './components/TheSidebar'
+//import TheSidebar from './components/TheSidebar'
 import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
 import { initWallet  } from 'solana-wallets-vue'
 import { initWorkspace } from '@/composables'
@@ -20,10 +20,11 @@ initWorkspace()
     <div class="w-full max-w-3xl lg:max-w-4xl mx-auto">
 
         <!-- Sidebar. -->
-        <the-sidebar class="py-4 md:py-8 md:pl-4 md:pr-8 fixed w-20 md:w-64"></the-sidebar>
+        <!--<the-sidebar class="py-4 md:py-8 md:pl-4 md:pr-8 fixed w-20 md:w-64"></the-sidebar>-->
 
         <!-- Main -->
-        <main class="flex-1 border-r border-l ml-20 md:ml-64 min-h-screen">
+        <!--<main class="flex-1 border-r border-l ml-20 md:ml-64 min-h-screen">-->
+        <main class="flex-1 border-r border-l">
             <header class="flex space-x-6 items-center justify-between px-8 py-4 border-b">
                 <div class="text-xl font-bold" v-text="route.name"></div>
                 <div class="">
